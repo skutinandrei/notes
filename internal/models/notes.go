f34871137bfbcab7a46afafc9e580058b1df640c
Elliot <skutinandrei@yandex.ru>
2026-04-15 19:47:02 +0300

    add json struct tags

--- internal/models/notes.go
+++ internal/models/notes.go
@@ -1,13 +1,13 @@
 package models
 
 type Note struct {
-	Id        int    `db:"id"`
-	Title     string `db:"title"`
-	Content   string `db:"content"`
-	CreatedAt string `db:"created_at"`
+	Id        int    `json:"id" db:"id"`
+	Title     string `json:"title" db:"title"`
+	Content   string `json:"content" db:"content"`
+	CreatedAt string `json:"created_at" db:"created_at"`
 }
 
 type CreateNoteInput struct {
-	Title   string `db:"title"`
-	Content string `db:"content"`
+	Title   string `json:"title" db:"title"`
+	Content string `json:"content" db:"content"`
 }
